9f85487f949c482ee5561f88bd2810a9e93443be
Simon Senger <76154207+Eb0nheart@users.noreply.github.com>
2024-02-05 21:38:54 +0100

    fjern scrap project

--- WebApplication1/DTOs.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Skycave.MessageService;
-
-public record GetMessages(int Page, int PageSize);
-
-public record CreateMessage(string Creator, string Message);
-
-public record UpdateMessage(Guid Id, string UpdatedMessage);
